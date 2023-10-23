9df71e384f7afdb90ff5a2e68999633e78e304ae
Heorhii <heorhii.berezovskyi@nure.ua>
2023-10-23 14:13:37 +0300

    Simplidy dataset initialization.

--- src/train/train.py
+++ src/train/train.py
@@ -1,7 +1,6 @@
 import json
 import os
 import random
-from typing import Tuple
 
 import lightning.pytorch as pl
 import numpy as np
@@ -10,24 +9,16 @@ from pytorch_lightning.loggers import CSVLogger, TensorBoardLogger
 from torch.utils.data import DataLoader
 
 from src.data.AirbusShipDetectionDataset import AirbusShipDetectionDataset
-from src.data.ShipCenteredCrop import ShipCenteredCrop
+from src.data.ResizeImageMask import ResizeImageMask
+from src.data.ResizeImageOnly import ResizeImageOnly
 from src.model.Unet import Unet
 from src.train.AirbusShipDetectorTrainingWrapper import AirbusShipDetectorTrainingWrapper
 
 
-def init_train_val_from_meta_info(data_root: str) -> Tuple[AirbusShipDetectionDataset, AirbusShipDetectionDataset]:
-    images_dir = os.path.join(data_root, 'train_v2')
-    train_file = os.path.join(data_root, 'train.json')
-    with open(train_file, 'r') as file:
-        train_state = json.load(file)
-
-    val_file = os.path.join(data_root, 'val.json')
-    with open(val_file, 'r') as file:
-        val_state = json.load(file)
-
-    train = AirbusShipDetectionDataset.from_state(state=train_state, images_dir=images_dir)
-    val = AirbusShipDetectionDataset.from_state(state=val_state, images_dir=images_dir)
-    return train, val
+def init_from_meta_info(images_dir: str, annotations_json: str) -> AirbusShipDetectionDataset:
+    with open(annotations_json, 'r') as file:
+        state = json.load(file)
+    return AirbusShipDetectionDataset.from_state(state=state, images_dir=images_dir)
 
 
 def worker_init_fn(worker_id):
@@ -40,19 +31,30 @@ def worker_init_fn(worker_id):
 if __name__ == '__main__':
     # 4_5 - focal + dice
     # 6_7 - + bce
+    # 8_9 - resize to 256
+    # 10_11 - random resize from 768 to 384 and ship centered crop
+    # 12_13 - random resize from 768 to 384 and random crop
+    # 14_15 - random resize from 768 to 256 and ship centered crop
 
     unet = Unet(init_channels=32, residual_block=True, inference=False)
     detector = AirbusShipDetectorTrainingWrapper(unet)
 
-    train_dataset, val_dataset = init_train_val_from_meta_info(r'D:\Data\airbus-ship-detection')
+    images_root = r'D:\Data\airbus-ship-detection\train_v2'
+    train_dataset = init_from_meta_info(images_dir=images_root,
+                                        annotations_json=r'D:\Data\airbus-ship-detection\train.json')
+    val_dataset = init_from_meta_info(images_dir=images_root,
+                                      annotations_json=r'D:\Data\airbus-ship-detection\val.json')
 
     # Set centered crop transform to train dataset, perform validation in original size.
-    transform = ShipCenteredCrop(hw=(256, 256), center_crop_random_shift=0.3)
-    train_dataset.set_sample_transform(transform)
+    # crop = ShipCenteredCrop(hw=(256, 256), center_crop_random_shift=0.3)
+    # crop = RandomCrop(hw=(256, 256))
+    # train_dataset.set_sample_transform(RandomResizeCrop(max_resize_dim=256, crop=crop))
+    train_dataset.set_sample_transform(ResizeImageMask((256, 256)))
+    val_dataset.set_sample_transform(ResizeImageOnly((256, 256)))
 
     train_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True, num_workers=4, pin_memory=True,
                               persistent_workers=True, worker_init_fn=worker_init_fn)
-    val_loader = DataLoader(dataset=val_dataset, batch_size=16, shuffle=True, num_workers=4, pin_memory=True,
+    val_loader = DataLoader(dataset=val_dataset, batch_size=16, shuffle=False, num_workers=4, pin_memory=True,
                             persistent_workers=True)
 
     trainer = pl.Trainer(max_epochs=5, logger=[CSVLogger(os.getcwd()), TensorBoardLogger(os.getcwd())],
