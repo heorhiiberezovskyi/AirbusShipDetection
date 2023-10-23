ba58418d02490a7186700ae692e3a9e8097faf36
Heorhii <heorhii.berezovskyi@nure.ua>
2023-10-23 13:47:14 +0300

    Implement ResizeImageOnly sample transform.

---
+++ src/data/ResizeImageOnly.py
@@ -0,0 +1,15 @@
+from typing import Tuple
+
+import cv2
+from numpy import ndarray
+
+from src.data.SampleTransform import SampleTransform
+
+
+class ResizeImageOnly(SampleTransform):
+    def __init__(self, hw: Tuple[int, int]):
+        self._hw = hw
+
+    def apply(self, image: ndarray, mask: ndarray) -> Tuple[ndarray, ndarray]:
+        result_image = cv2.resize(image, (self._hw[1], self._hw[0]), interpolation=cv2.INTER_AREA)
+        return result_image, mask
