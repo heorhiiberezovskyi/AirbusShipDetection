a0f45bb7ac9584570153afb10d107455806d8e57
Heorhii <heorhii.berezovskyi@nure.ua>
2023-10-25 17:49:17 +0300

    Add training entry point.

--- src/train/train.py
+++ src/train/train.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 import random
@@ -30,28 +31,18 @@ def worker_init_fn(worker_id):
     random.seed(seed)
 
 
-if __name__ == '__main__':
-    # sampling: balanced
-    # 4_5 - focal + dice
-    # 6_7 - + bce
-    # 8_9 - resize to 256
-    # 10_11 - random resize from 768 to 384 and ship centered crop
-    # 12_13 - random resize from 768 to 384 and random crop
-    # 14_15 - random resize from 768 to 256 and ship centered crop
-    # 20_21 - resize to 256
-    # 22_23 - resize to 256, random sampling
-
+def main(args):
     unet = Unet(init_channels=32, residual_block=True)
     detector = AirbusShipDetectorTrainingWrapper(unet)
 
-    images_root = r'D:\Data\airbus-ship-detection\train_v2'
-    train_dataset = init_from_meta_info(images_dir=images_root,
-                                        annotations_json=r'D:\Data\airbus-ship-detection\train.json',
+    train_dataset = init_from_meta_info(images_dir=args.images_dir,
+                                        annotations_json=args.train_annotations_json,
                                         balanced_sampling=False)
-    val_dataset = init_from_meta_info(images_dir=images_root,
-                                      annotations_json=r'D:\Data\airbus-ship-detection\val.json',
+    val_dataset = init_from_meta_info(images_dir=args.images_dir,
+                                      annotations_json=args.val_annotations_json,
                                       balanced_sampling=True)
 
+    # TODO: Implement transform builder to support different train and val transforms during training.
     # Set centered crop transform to train dataset, perform validation in original size.
     # crop = ShipCenteredCrop(hw=(256, 256), center_crop_random_shift=0.3)
     # crop = RandomCrop(hw=(256, 256))
@@ -69,3 +60,24 @@ if __name__ == '__main__':
                          val_check_interval=0.25, log_every_n_steps=10,
                          limit_val_batches=200)
     trainer.fit(model=detector, train_dataloaders=train_loader, val_dataloaders=val_loader)
+
+
+if __name__ == '__main__':
+    # sampling: balanced
+    # 4_5 - focal + dice
+    # 6_7 - + bce
+    # 10_11 - random resize from 768 to 384 and ship centered crop
+    # 12_13 - random resize from 768 to 384 and random crop
+    # 14_15 - random resize from 768 to 256 and ship centered crop
+    # 20_21 - resize to 256
+    # 22_23 - resize to 256, random sampling
+
+    parser = argparse.ArgumentParser(description='Train for 5 epochs.')
+    parser.add_argument("--images_dir", type=str, help="Directory with images to train on.")
+    parser.add_argument("--train_annotations_json", type=str,
+                        help="Path to JSON file with train ship segmentations annotations")
+    parser.add_argument("--val_annotations_json", type=str,
+                        help="Path to JSON file with validation ship segmentations annotations")
+
+    _args = parser.parse_args()
+    main(_args)
