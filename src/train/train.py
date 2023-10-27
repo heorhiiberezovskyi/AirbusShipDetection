ad17645b189cc356afc963bf6c0abe1fa2244b8f
Heorhii <heorhii.berezovskyi@nure.ua>
2023-10-27 16:09:07 +0300

    Add support of tf32_high precision.

--- src/train/train.py
+++ src/train/train.py
@@ -10,8 +10,8 @@ from pytorch_lightning.loggers import CSVLogger, TensorBoardLogger
 from torch.utils.data import DataLoader
 
 from src.data.AirbusShipDetectionDataset import AirbusShipDetectionDataset
-from src.data.sample_transform.ResizeImageMask import ResizeImageMask
-from src.data.sample_transform.ResizeImageOnly import ResizeImageOnly
+from src.data.sample_transform.RandomResizeCrop import RandomResizeCrop
+from src.data.sample_transform.ShipCenteredCrop import ShipCenteredCrop
 from src.model.Unet import Unet
 from src.train.AirbusShipDetectorTrainingWrapper import AirbusShipDetectorTrainingWrapper
 
@@ -32,6 +32,11 @@ def worker_init_fn(worker_id):
 
 
 def main(args):
+    if args.precision == 'tf32_high':
+        torch.set_float32_matmul_precision(args.precision)
+    elif args.precision != 'fp32':
+        raise NotImplementedError('')
+
     unet = Unet(init_channels=32, residual_block=True)
     detector = AirbusShipDetectorTrainingWrapper(unet)
 
@@ -44,11 +49,11 @@ def main(args):
 
     # TODO: Implement transform builder to support different train and val transforms during training.
     # Set centered crop transform to train dataset, perform validation in original size.
-    # crop = ShipCenteredCrop(hw=(256, 256), center_crop_random_shift=0.3)
+    crop = ShipCenteredCrop(hw=(256, 256), center_crop_random_shift=0.3)
     # crop = RandomCrop(hw=(256, 256))
-    # train_dataset.set_sample_transform(RandomResizeCrop(max_resize_dim=256, crop=crop))
-    train_dataset.set_sample_transform(ResizeImageMask((256, 256)))
-    val_dataset.set_sample_transform(ResizeImageOnly((256, 256)))
+    train_dataset.set_sample_transform(RandomResizeCrop(max_resize_dim=256, crop=crop))
+    # train_dataset.set_sample_transform(ResizeImageMask((256, 256)))
+    # val_dataset.set_sample_transform(ResizeImageOnly((256, 256)))
 
     train_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True, num_workers=4, pin_memory=True,
                               persistent_workers=True, worker_init_fn=worker_init_fn)
@@ -71,6 +76,7 @@ if __name__ == '__main__':
     # 14_15 - random resize from 768 to 256 and ship centered crop
     # 20_21 - resize to 256
     # 22_23 - resize to 256, random sampling
+    # 24_25 - random resize from 768 to 256 and ship centered crop
 
     parser = argparse.ArgumentParser(description='Train for 5 epochs.')
     parser.add_argument("--images_dir", type=str, help="Directory with images to train on.")
@@ -78,6 +84,9 @@ if __name__ == '__main__':
                         help="Path to JSON file with train ship segmentations annotations")
     parser.add_argument("--val_annotations_json", type=str,
                         help="Path to JSON file with validation ship segmentations annotations")
+
+    parser.add_argument("--precision", type=str, help="Precision to train", default='fp32',
+                        choices=['fp32', 'tf32_high'])
 
     _args = parser.parse_args()
     main(_args)
