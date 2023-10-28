3892413658528e3b91861ca69dbdd19ccc6e415d
Heorhii <heorhii.berezovskyi@nure.ua>
2023-10-28 14:33:50 +0300

    Add support of tf32 medium precision. Specify sampling type through entry point argument.

--- src/train/train.py
+++ src/train/train.py
@@ -10,8 +10,8 @@ from pytorch_lightning.loggers import CSVLogger, TensorBoardLogger
 from torch.utils.data import DataLoader
 
 from src.data.AirbusShipDetectionDataset import AirbusShipDetectionDataset
-from src.data.sample_transform.RandomResizeCrop import RandomResizeCrop
-from src.data.sample_transform.ShipCenteredCrop import ShipCenteredCrop
+from src.data.sample_transform.ResizeImageMask import ResizeImageMask
+from src.data.sample_transform.ResizeImageOnly import ResizeImageOnly
 from src.model.Unet import Unet
 from src.train.AirbusShipDetectorTrainingWrapper import AirbusShipDetectorTrainingWrapper
 
@@ -35,6 +35,9 @@ def main(args):
     if args.precision == 'tf32_high':
         print('Training in tf32 high precision')
         torch.set_float32_matmul_precision('high')
+    elif args.precision == 'tf32_medium':
+        print('Training in tf32 medium precision')
+        torch.set_float32_matmul_precision('medium')
     elif args.precision != 'fp32':
         print('Training in fp32 precision')
         raise NotImplementedError('')
@@ -42,20 +45,27 @@ def main(args):
     unet = Unet(init_channels=32, residual_block=True)
     detector = AirbusShipDetectorTrainingWrapper(unet)
 
+    if args.sampling == 'random':
+        balanced_sampling = False
+    elif args.sampling == 'balanced':
+        balanced_sampling = True
+    else:
+        raise NotImplementedError('')
+
     train_dataset = init_from_meta_info(images_dir=args.images_dir,
                                         annotations_json=args.train_annotations_json,
-                                        balanced_sampling=False)
+                                        balanced_sampling=balanced_sampling)
     val_dataset = init_from_meta_info(images_dir=args.images_dir,
                                       annotations_json=args.val_annotations_json,
                                       balanced_sampling=True)
 
     # TODO: Implement transform builder to support different train and val transforms during training.
     # Set centered crop transform to train dataset, perform validation in original size.
-    crop = ShipCenteredCrop(hw=(256, 256), center_crop_random_shift=0.3)
+    # crop = ShipCenteredCrop(hw=(256, 256), center_crop_random_shift=0.3)
     # crop = RandomCrop(hw=(256, 256))
-    train_dataset.set_sample_transform(RandomResizeCrop(max_resize_dim=256, crop=crop))
-    # train_dataset.set_sample_transform(ResizeImageMask((256, 256)))
-    # val_dataset.set_sample_transform(ResizeImageOnly((256, 256)))
+    # train_dataset.set_sample_transform(RandomResizeCrop(max_resize_dim=256, crop=crop))
+    train_dataset.set_sample_transform(ResizeImageMask((256, 256)))
+    val_dataset.set_sample_transform(ResizeImageOnly((256, 256)))
 
     train_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True, num_workers=4, pin_memory=True,
                               persistent_workers=True, worker_init_fn=worker_init_fn)
@@ -78,18 +88,20 @@ if __name__ == '__main__':
     # 14_15 - random resize from 768 to 256 and ship centered crop
     # 20_21 - resize to 256
     # 22_23 - resize to 256, random sampling
-    # 24_25 - random resize from 768 to 256 and ship centered crop
-    # 26_27 - resize to 256, tf_32 high
+    # 24_25 - random resize from 768 to 256 and ship centered crop (sampling random)
+    # 25_26 - random resize from 768 to 256 and ship centered crop (sampling balanced)
 
     parser = argparse.ArgumentParser(description='Train for 5 epochs.')
     parser.add_argument("--images_dir", type=str, help="Directory with images to train on.")
+    parser.add_argument("--sampling", type=str, help="Random or balanced", default='random',
+                        choices=['random', 'balanced'])
     parser.add_argument("--train_annotations_json", type=str,
                         help="Path to JSON file with train ship segmentations annotations")
     parser.add_argument("--val_annotations_json", type=str,
                         help="Path to JSON file with validation ship segmentations annotations")
 
     parser.add_argument("--precision", type=str, help="Precision to train", default='fp32',
-                        choices=['fp32', 'tf32_high'])
+                        choices=['fp32', 'tf32_high', 'tf32_medium'])
 
     _args = parser.parse_args()
     main(_args)
